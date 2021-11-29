028ac98128d9abe425b7fb2a22297a1776502f8c
Octave Larose <octave.larose@hotmail.fr>
2021-11-29 11:26:22 +0000

    Creates a node tree from the graph

--- ruby_black_magic/rb_adventures.py
+++ ruby_black_magic/rb_adventures.py
@@ -1,6 +1,24 @@
 import sys
+from treelib import Node, Tree
 
 from rubybridge import RubySession
+
+
+def get_node_tree(graph):
+    node_tree = Tree()
+    parent_node_set = False
+
+    for edge in graph.edges():
+        parent_node_id, node_id = [node.id() for node in edge.nodes()]
+        if not parent_node_set:
+            node_tree.create_node(parent_node_id, parent_node_id)
+            parent_node_set = True
+        node_tree.create_node(node_id, node_id, parent=parent_node_id)
+
+    for node_id, node in graph.nodes().items():
+        node_tree[node_id].tag = node.props()['label']
+
+    return node_tree
 
 
 def main(argv: [str]):
@@ -22,18 +40,25 @@ def main(argv: [str]):
         name = bgv_parser.graph_name(graph_header)
         graph = bgv_parser.read_graph()
 
-        print("###")
-        # print("GRAPH_HEADER:", graph_header)
-        print("NAME:", name)
-        print("GRAPH:", graph)
-        # print("PROPS:", graph.props())
-        # print("NODES:", graph.nodes())
-        print("NODE LABELS:")
-        for node in graph.nodes().values():
-            print(node.props()['label'])
-        print("EDGES:", graph.edges())
-        print("BLOCKS:", graph.blocks())
-        print("###")
+        node_tree = get_node_tree(graph)
+        node_tree.show()
+
+
+        # for x in new_tree:
+        #     print(x)
+
+        # print("###")
+        # # print("GRAPH_HEADER:", graph_header)
+        # print("NAME:", name)
+        # print("GRAPH:", graph)
+        # # print("PROPS:", graph.props())
+        # # print("NODES:", graph.nodes())
+        # print("NODE LABELS:")
+        # for node in graph.nodes().values():
+        #     print(node.props()['label'])
+        # print("EDGES:", graph.edges())
+        # print("BLOCKS:", graph.blocks())
+        # print("###")
 
 
 if __name__ == "__main__":
