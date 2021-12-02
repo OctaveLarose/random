6958d553b2db01e11b680ae9b069b52a7b634c00
Octave Larose <octave.larose@hotmail.fr>
2021-12-02 10:21:37 +0000

    Day 2 part 2

--- adventofcode2021/day2/day2.py
+++ adventofcode2021/day2/day2.py
@@ -14,11 +14,27 @@ def part1(instrs: [(str, int)]) -> (int, int):
     return h, d
 
 
+def part2(instrs: [(str, int)]) -> (int, int):
+    h, d, a = 0, 0, 0
+
+    for dir, val in instrs:
+        if dir == "forward":
+            h = h + val
+            d = d + a * val
+        elif dir == "down":
+            a = a + val
+        elif dir == "up":
+            a = a - val
+
+    return h, d
+
+
 def main():
     instrs = [(line[:-1].split()[0], int(line[:-1].split()[1])) for line in open("input2", 'r').readlines()]
 
     print(instrs)
     print("Part 1:", part1(instrs))
+    print("Part 2:", part2(instrs))
 
 
 if __name__ == "__main__":
