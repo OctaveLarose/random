e5ca871a4dcc278e8f72205c7e99a6860db20c06
Octave Larose <octave.larose@hotmail.fr>
2021-12-03 10:43:32 +0000

    Using zip to save lines

--- adventofcode2021/day3/day3.py
+++ adventofcode2021/day3/day3.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python
 
 def part1(values: [[str]]):
-    bl = [[] for _ in range(len(values[0]))]
-    for val in values:
-        for idx, n in enumerate(val):
-            bl[idx].append(n)
-
-    gamma_str = ''.join(list(map(lambda lst: max(set(lst), key=lst.count), bl)))
+    gamma_str = ''.join(list(map(lambda lst: max(set(lst), key=lst.count), list(zip(*values)))))
     epsilon_str = ''.join('0' if c == '1' else '1' for c in gamma_str)
     return int(gamma_str, 2) * int(epsilon_str, 2)
 
 
 def main():
     values = [list(line[:-1]) for line in open("input3", 'r').readlines()]
-
     print(f"Part 1: {part1(values)}")
 
 
