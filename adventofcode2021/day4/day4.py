3a5bf8aab4562e2a0f972eb8b8c3424d94d769d8
OctaveLarose <octave.larose@hotmail.fr>
2021-12-05 16:02:17 +0000

    Part 1

--- adventofcode2021/day4/day4.py
+++ adventofcode2021/day4/day4.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python
+from pprint import pprint
+
+GRID_SIZE = 5
 
 
-def parse(filename: str) -> ([int], [[int]]):
+def parse(filename: str) -> ([int], [[int, bool]]):
     with open(filename, 'r') as f:
         draws = [int(n) for n in f.readline().strip().split(",")]
-        grids = [[(int(val), False) for val in (''.join([f.readline() for _ in range(5)])).strip().split()] for _ in f.readline()]  # Blank line skip with readline
+        grids = []
+        while f.readline():
+            grids.append([[int(val), False] for val in (''.join([f.readline() for _ in range(GRID_SIZE)])).strip().split()])
     return draws, grids
 
 
+def has_won(grid: [(int, bool)]) -> bool:
+    return any([all(r) for r in [[c for n, c in grid[i:i+GRID_SIZE]] for i in range(GRID_SIZE)]]) or \
+           any([all(c) for c in [[c for n, c in grid[i:GRID_SIZE * GRID_SIZE:GRID_SIZE]] for i in range(GRID_SIZE)]])
+
+
+def part1(draws: [int], grids: [[int]]) -> int:
+    for d in draws:
+        for g in grids:
+            for idx, (n, c) in enumerate(g):
+                g[idx][1] = (n == d) or g[idx][1]
+            if has_won(g):
+                return sum([n for (n, c) in g if not c]) * d
+
+
 def main():
-    draws, grids = parse("testinput4.txt")
+    draws, grids = parse("input4.txt")
 
-    print(f"Draws: {draws}")
-    print(f"Grids: {grids}")
-
-    # values = [list(line[:-1]) for line in open("input3", 'r').readlines()]
-    # print(f"Part 1: {part1(values)}")
-    # print(f"Part 2: {part2(values)}")
+    print(f"Part 1: {part1(draws, grids)}")
 
 
 if __name__ == "__main__":
