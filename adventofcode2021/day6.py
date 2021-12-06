d8fc99ab71f8a692089aa8a7bf5375c4a60d1638
Octave Larose <octave.larose@hotmail.fr>
2021-12-06 10:31:19 +0000

    day 6, optimized solution

--- adventofcode2021/day6.py
+++ adventofcode2021/day6.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
+from collections import Counter, OrderedDict
 
 
-def part1(lfs: [int], nbr_days: int) -> int:
+def count_lanternfish(lfs: {int: int}, nbr_days: int) -> int:
     for i in range(nbr_days):
-        for idx, f in enumerate(lfs):
-            lfs[idx] = lfs[idx] - 1
-            if lfs[idx] == -1:
-                lfs[idx] = 6
-                lfs.append(9)
-    return len(lfs)
+        for k, v in list(lfs.items())[1:]:
+            lfs[k - 1] += v
+            lfs[k] = 0
+        lfs[8] = lfs[-1]
+        lfs[6] += lfs[-1]
+        lfs[-1] = 0
+
+    return sum(v for v in lfs.values())
 
 
 def main():
-    lfs = [int(v) for v in open("inputs/tests/testinput6", 'r').readline().split(",")]
+    lfs = OrderedDict((i, 0) for i in range(-1, 9))
+    lfs.update(Counter([int(v) for v in open("inputs/input6", 'r').readline().split(",")]))
 
-    print("Part 1:", part1(lfs, 80))
+    print("Part 1:", count_lanternfish(lfs.copy(), 80))
+    print("Part 2:", count_lanternfish(lfs.copy(), 256))
 
 
 if __name__ == "__main__":
