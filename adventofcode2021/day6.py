bb291c5fd1b718497d78692f1eb4cf20a4241a89
Octave Larose <octave.larose@hotmail.fr>
2021-12-06 09:50:30 +0000

    Unoptimized part 1

--- adventofcode2021/day6.py
+++ adventofcode2021/day6.py
@@ -2,14 +2,19 @@
 
 
 def part1(lfs: [int], nbr_days: int) -> int:
-    print(lfs)
-    return 42
+    for i in range(nbr_days):
+        for idx, f in enumerate(lfs):
+            lfs[idx] = lfs[idx] - 1
+            if lfs[idx] == -1:
+                lfs[idx] = 6
+                lfs.append(9)
+    return len(lfs)
 
 
 def main():
     lfs = [int(v) for v in open("inputs/tests/testinput6", 'r').readline().split(",")]
 
-    print("Part 1:", part1(lfs, 18))
+    print("Part 1:", part1(lfs, 80))
 
 
 if __name__ == "__main__":
