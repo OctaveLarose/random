46f062a3bc8e22cda0fc4096df0b97effacb8db7
OctaveLarose <octave.larose@hotmail.fr>
2021-12-07 09:48:38 +0000

    Day 7 part 2 solution ; pretty weak but works surprisingly well

--- adventofcode2021/day7.py
+++ adventofcode2021/day7.py
@@ -2,16 +2,18 @@
 
 
 def part1(crabs) -> int:
-    fuel_costs = []
-    for i in range(min(crabs), max(crabs)):
-        fuel_costs.append(sum([abs(c - i) for c in crabs]))
-    return min(fuel_costs)
+    return min([sum([abs(c - i) for c in crabs]) for i in range(min(crabs), max(crabs))])
+
+
+def part2(crabs) -> int:
+    return min([sum([abs(c - i) * (abs(c - i) + 1) // 2 for c in crabs]) for i in range(min(crabs), max(crabs))])
 
 
 def main():
     crabs = [int(v) for v in open("inputs/input7", 'r').readline().split(",")]
 
     print("Part 1:", part1(crabs))
+    print("Part 2:", part2(crabs))
 
 
 if __name__ == "__main__":
