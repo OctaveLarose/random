936605d75f1880b5547fa7069f1e0f8e49ed9725
Octave Larose <octave.larose@hotmail.fr>
2021-12-08 14:16:46 +0000

    day 8 part 1

--- adventofcode2021/day8.py
+++ adventofcode2021/day8.py
@@ -2,14 +2,18 @@
 
 
 def part1(vals) -> int:
-    print(vals)
-    return 42
+    return len([x for x in [y for x in [v[1] for v in vals] for y in x] if len(x) in [2, 3, 4, 7]])
+
+
+def part2(vals) -> int:
+    pass
 
 
 def main():
-    vals = [tuple([c.strip().split(" ") for c in l.split("|")]) for l in open("inputs/tests/testinput8", 'r').readlines()]
+    vals = [tuple([c.strip().split(" ") for c in l.split("|")]) for l in open("inputs/input8", 'r').readlines()]
 
     print("Part 1:", part1(vals))
+    print("Part 2:", part2(vals))
 
 
 if __name__ == "__main__":
