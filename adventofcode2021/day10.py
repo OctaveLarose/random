3fd8473b3ecee4e4fe19cb609f9d8e86881c037c
Octave Larose <octave.larose@hotmail.fr>
2021-12-10 19:03:38 +0000

    day 10 part 2, first solution

--- adventofcode2021/day10.py
+++ adventofcode2021/day10.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
+from pprint import pprint
 
 
 def get_corrupted_char_in_line(line) -> (bool, str):
-    pairs = ['()', '[]', '{}', '<>']
     stack = []
     for c in line:
         stack.append(c)
 
         while len(stack) != 0 and stack[-1] in ")]}>":
-            if ''.join(stack[-2:]) in pairs:
+            if ''.join(stack[-2:]) in ['()', '[]', '{}', '<>']:
                 stack = stack[:-2]
             else:
                 return c
@@ -24,10 +24,40 @@ def part1(lines) -> int:
     return sum([{')': 3, ']': 57, '}': 1197, '>': 25137, None: 0}.get(c) for c in invalid_chars])
 
 
+def part2(lines) -> int:
+    missing_vals = []
+
+    for l in lines:
+        if get_corrupted_char_in_line(l) is not None:
+            continue
+        stack = []
+        for c in l:
+            stack.append(c)
+
+            while len(stack) != 0 and stack[-1] in ")]}>":
+                if ''.join(stack[-2:]) in ['()', '[]', '{}', '<>']:
+                    stack = stack[:-2]
+                else:
+                    return c
+
+        missing_vals.append(list(reversed([{'(': ')', '[': ']', '{': '}', '<': '>'}.get(c) for c in stack])))
+
+    total_points = []
+    for m in missing_vals:
+        score = 0
+        for c in m:
+            score *= 5
+            score += {')': 1, ']': 2, '}': 3, '>': 4}.get(c)
+        total_points.append(score)
+
+    return sorted(total_points)[len(total_points) // 2]
+
+
 def main():
-    lines = [list(l.strip()) for l in open("inputs/tests/testinput10", 'r').readlines()]
+    lines = [list(l.strip()) for l in open("inputs/input10", 'r').readlines()]
 
     print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
 
 
 if __name__ == "__main__":
