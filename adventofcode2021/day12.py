3d92e0e89baa7330a1701ed579b18014555225e7
OctaveLarose <octave.larose@hotmail.fr>
2021-12-13 16:36:32 +0000

    Working, and fast, day 12 part 2

--- adventofcode2021/day12.py
+++ adventofcode2021/day12.py
@@ -13,14 +13,9 @@ def get_connected_rooms(room, prev_path, conns, max_small_visits):
         if conn_room == "start":
             continue
 
-        if conn_room.islower():
-            if max_small_visits == 1 and conn_room in prev_path:
+        if conn_room.islower() and conn_room in prev_path:
+            if max_small_visits == 1:
                 continue
-            else:
-                if conn_room in prev_path:
-                    c = Counter(prev_path)
-                    if any([a.islower() and b >= max_small_visits for a, b in c.items()]):
-                        continue
 
         connected_rooms.append(conn_room)
     return connected_rooms
@@ -29,18 +24,19 @@ def get_connected_rooms(room, prev_path, conns, max_small_visits):
 def navigate(room, prev_path, conns, paths, max_small_visits):
     if room == "end":
         return prev_path + ["end"]
+
+    if room.islower() and room in prev_path and any([a.islower() and b >= max_small_visits for a, b in Counter(prev_path).items()]):
+        return
+
     for cr in get_connected_rooms(room, prev_path, conns, max_small_visits):
         n = navigate(cr, prev_path + [room], conns, paths, max_small_visits)
-        # if not any([isinstance(v, list) for v in n]):
         paths.append(n)
-    return paths
 
 
 def navigator(conns, max_small_visits=1):
-    paths = navigate("start", [], conns, [], max_small_visits)
-    paths = list(filter(None, map(lambda l: None if any([type(x) is list for x in l]) else l, paths)))
-    # print(paths)
-    return len(paths)
+    p = []
+    navigate("start", [], conns, p, max_small_visits)
+    return len(list(filter(None, p)))
 
 
 def main():
