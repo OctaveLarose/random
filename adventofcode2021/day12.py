de1acbce21a88dfcbeb57ec644f237e3024af89d
OctaveLarose <octave.larose@hotmail.fr>
2021-12-13 09:09:40 +0000

    day 12 part 1: finds all paths but doesn't store them in a clean uniform list

--- adventofcode2021/day12.py
+++ adventofcode2021/day12.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python
-
-def get_connected_rooms(room, previous_room, conns):
-    return list(filter(lambda r: r is None or r == previous_room, [c[1] if c[0] == room else (c[0] if c[1] == room else None) for c in conns]))
+from pprint import pprint
 
 
-def navigate(room, previous_room, conns):
+def get_connected_rooms(room, prev_path, conns):
+    connected_rooms = []
+    for c in conns:
+        if room not in c:
+            continue
+
+        conn_room = c[0] if c[1] == room else c[1]
+
+        if conn_room.islower() and conn_room in prev_path:
+            continue
+
+        connected_rooms.append(conn_room)
+    return connected_rooms
+
+
+def navigate(room, prev_path, conns):
     paths = []
 
     if room == "end":
-        return ["end"]
-    for cr in get_connected_rooms(room, previous_room, conns):
-        paths.append([room] + navigate(cr, room, conns))
+        return prev_path + ["end"]
+    for cr in get_connected_rooms(room, prev_path, conns):
+        paths.append(navigate(cr, prev_path + [room], conns))
     return paths
 
 
 def part1(conns):
-    print(conns)
-
-    print(navigate("start", None, conns))
+    pprint(navigate("start", [], conns))
     return 42
 
 
 def main():
     conns = [tuple(v.strip().split("-")) for v in open("inputs/tests/testinput12", 'r').readlines()]
-    rooms = set([room for pair in conns for room in pair])
 
     print("Part 1:", part1(conns))
 
